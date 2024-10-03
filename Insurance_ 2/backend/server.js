5b64ecac8dfe9d135020e20d8d92e51a66fe540b
Harshit Maurya <130442677+mrharsh1@users.noreply.github.com>
2024-10-03 14:20:19 +0530

    Update server.js

--- Insurance_ 2/backend/server.js
+++ Insurance_ 2/backend/server.js
@@ -98,7 +98,7 @@ app.delete('/api/delete/:ID', async (req, res) => {
         `https://creator.zoho.in/api/v2/dev_it/my-first-project/report/All_Leave_Requests/${ID}`,  // Add the ID to the URL
         {
           headers: {
-            Authorization: `Zoho-oauthtoken 1000.d12b7350ccdc199cbe469bd9c6a9a0b8.e851843f1cb30497d9a9444216447cec`,
+            Authorization: `Zoho-oauthtoken ${process.env.ZOHO_ACCESS_TOKEN}`,
           },
           timeout: 10000
         }
@@ -119,7 +119,7 @@ app.put('/api/update/:ID', async (req, res) => {
         { data: req.body }, // Data from the request body to update
         {
           headers: {
-            Authorization: `Zoho-oauthtoken 1000.d12b7350ccdc199cbe469bd9c6a9a0b8.e851843f1cb30497d9a9444216447cec`,
+            Authorization: `Zoho-oauthtoken ${process.env.ZOHO_ACCESS_TOKEN}`,
             'Content-Type': 'application/json',
           },
           timeout: 10000
